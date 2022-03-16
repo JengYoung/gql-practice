e6aa660822c7abc558787899046f62d3ffe54284
JengYoung <young_pallete@naver.com>
2022-03-16 19:28:43 +0900

    :hammer: update type resolver
    타입 리졸버의 반환 데이터 변경

--- server/index.js
+++ server/index.js
@@ -1,22 +1,57 @@
 const { ApolloServer } = require('apollo-server');
 
+/*
+ * NOTE: 리졸버 함수에서는 정수, 문자열, 불리언 같은 값 외에도 객체 역시 반환이 가능하다.
+ */
 const typeDefs = `
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }  
+
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
+  }
+  
+  type Mutation {
+    postPhoto(name: String! description: String): Photo!
   }
 `
 
+let _id = 0;
+const photos = [];
+
 const resolvers = {
   Query: {
-    totalPhotos: () => 42
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos,
+  },
+
+  Mutation: {
+    // 리졸버 함수의 첫 번째 인자는 부모 객체에 대한 참조이다. (여기서는 Mutation)
+    // 두 번째 인자는 뮤테이션에 넣어 줄 GraphQL 인자이다.
+    postPhoto(parent, args) { 
+      const newPhoto = {
+        id: _id++,
+        ...args
+      }
+      
+      photos.push(newPhoto)
+
+      return newPhoto;
+    }
   }
 }
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 })
 
 server
   .listen()
   .then(({ url }) => console.log(`GraphQL Service running on ${url}`))
+
