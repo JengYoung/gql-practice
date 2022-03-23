2a7e06b3f5de7284a4745df660088da3924f2b62
JengYoung <young_pallete@naver.com>
2022-03-23 23:48:41 +0900

    :sparkles: set Request Header to auth valid user
    토큰 여부에 따라 유저를 판별하고 요청하도록 설정

--- client/src/graphql/client.js
+++ client/src/graphql/client.js
@@ -2,6 +2,14 @@ import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
   url: process.env.REACT_APP_API_END_POINT,
+  request: (operation) => {
+    operation.setContext((context) => ({
+      headers: {
+        ...context.headers,
+        authorization: localStorage.getItem("token"),
+      },
+    }));
+  },
 });
 
 export default client;
